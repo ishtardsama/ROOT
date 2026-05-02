168ebcb36639ac4a6f96563f0ee5f0e64965775f
ishtardsama <noteethme@gmail.com>
2026-05-02 15:56:38 +0800

    battery stuff

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,15 @@ IPAddress gateway(192, 168, 222, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 const int MOISTURE_PIN = 2; 
+const int BATTERY_PIN = 34; 
 BH1750 lightMeter(0x23);
 WebServer server(80);
 
+unsigned long lastBeat = 0; 
+
 void handleData() {
+    lastBeat = millis(); 
+
     int rawMoisture = analogRead(MOISTURE_PIN);
     int moisturePercent = map(rawMoisture, 2730, 1350, 0, 100);
     moisturePercent = constrain(moisturePercent, 0, 100);
@@ -25,9 +30,17 @@ void handleData() {
     float lux = lightMeter.readLightLevel();
     if (lux < 0) lux = 0;
 
+    int rawBat = analogRead(BATTERY_PIN);
+
+    float batVoltage = (rawBat / 4095.0) * 3.3 * 2; 
+    
+    int batPercent = map(batVoltage * 100, 320, 420, 0, 100);
+    batPercent = constrain(batPercent, 0, 100);
+
     StaticJsonDocument<256> doc;
     doc["moisture"] = moisturePercent;
     doc["light"] = (int)lux; 
+    doc["battery"] = batPercent; 
     doc["status"] = "OK";
 
     String response;
@@ -36,13 +49,13 @@ void handleData() {
     server.sendHeader("Access-Control-Allow-Origin", "*");
     server.send(200, "application/json", response);
 
-    Serial.printf("ROOT: M %d%% | L %d lx\n", moisturePercent, (int)lux);
+    Serial.printf("ROOT: M %d%% | L %d lx | B %d%%\n", moisturePercent, (int)lux, batPercent);
 }
 
 void setup() {
     Serial.begin(115200);
     delay(2000);
-    Serial.println("\n---SYSTEM  BOOTING ---");
+    Serial.println("\n--- SYSTEM BOOTING (Woke up from sleep or hard reset) ---");
 
     Wire.begin(); 
     delay(100);
@@ -51,13 +64,22 @@ void setup() {
         Serial.println("Static IP Error");
     }
     WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
+
+    int wifiAttempts = 0;
+    while (WiFi.status() != WL_CONNECTED && wifiAttempts < 20) {
         delay(500);
         Serial.print(".");
+        wifiAttempts++;
     }
-    Serial.println("\nWiFi Connected.");
 
-    //dk why i keep having erroes w light sensor connection
+    if (WiFi.status() == WL_CONNECTED) {
+        Serial.println("\nWiFi Connected.");
+    } else {
+        Serial.println("\nWiFi Failed. Going back to sleep to save power.");
+        esp_sleep_enable_timer_wakeup(30ULL * 60 * 1000000); 
+        esp_deep_sleep_start();
+    }
+
     delay(500); 
     if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
         Serial.println("BH1750 [ONLINE]");
@@ -72,15 +94,22 @@ void setup() {
     server.begin();
     Serial.print("App URL: http://");
     Serial.println(WiFi.localIP());
+    
+    lastBeat = millis(); 
 }
 
 void loop() {
     server.handleClient();
-    
-    static unsigned long lastBeat = 0;
-    if (millis() - lastBeat > 10000) {
-        Serial.println("System Active: Waiting for app response...");
-        lastBeat = millis();
+    if (millis() - lastBeat > 15000) {
+        Serial.println("App disconnected (no requests for 15s).");
+        
+        //database heere gua
+
+        Serial.println("Going to Deep Sleep for 30 minutes...");
+        
+        esp_sleep_enable_timer_wakeup(30ULL * 60 * 1000000); 
+        esp_deep_sleep_start();
     }
+    
     delay(10);
 }
